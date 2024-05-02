46fddcb83a3275dfb3787a1898e4aa95a0a03398
CisDeLocht <cisse.delocht@outlook.com>
2024-05-02 14:53:17 +0200

    Incorperated system model. Multiple antenna feature + normalized gaussian pilot sequences

--- cell_functions.py
+++ cell_functions.py
@@ -15,18 +15,19 @@ def populate_cell(grid, sparsity):
     indices = np.column_stack((i, j))
     indices_list = [tuple(index) for index in indices]
 
-    return grid, indices, indices_list
+    return grid, indices, indices_list, len(indices_list)
 
 
-def calculate_distances(grid, indices):
+def calculate_distances(grid, indices, M, N):
+    bs_height = 10
     grid_size = grid.shape[0]
     center = int((grid_size - 1) / 2)
     center_index = np.array([center, center])
-    distances2b = np.asarray([np.linalg.norm(center_index - index) for index in indices])
-    num_users = len(indices)
-    distance_matrix = np.array([[np.linalg.norm(indices[i] - indices[j]) for j in range(num_users)] for i in range(num_users)])
+    distances2b_horizontal = np.asarray([np.linalg.norm(center_index - index) for index in indices])
+    distances2b = np.sqrt(distances2b_horizontal**2 + bs_height**2)                                                     #Pythagoras
+    IU_distance_matrix = np.array([[np.linalg.norm(indices[i] - indices[j]) for j in range(N)] for i in range(N)])      #Inter-User distances
 
-    return distances2b, distance_matrix
+    return distances2b, IU_distance_matrix
 
 def generate_correlated_shadow_fading(distance_matrix, sigma):
     eps = 1e-10
@@ -38,17 +39,24 @@ def generate_correlated_shadow_fading(distance_matrix, sigma):
     D[np.abs(D) < eps] = 0
     SF = L @ np.sqrt(D) @ SF_uncorrelated
     return SF
-def simulate_path_loss_rayleigh(d2b, dm, f):
+def simulate_path_loss_rayleigh(d2b, dm, M, f):
     UMC_PL = 36.7*np.log10(d2b) + 22.7 + 26*np.log10(f)
     SF = generate_correlated_shadow_fading(dm, 4)
     PL_dB = UMC_PL + SF                                                                                  #sigma = 4 according to UMC model in TS 36.814
     PL = 10**(-PL_dB/10)
-    rayleigh_fading = np.random.normal(0, np.sqrt(0.5), size=d2b.shape[0]) + 1j * np.random.normal(0, np.sqrt(0.5), size=d2b.shape[0])
-    h = np.multiply(np.sqrt(PL) ,rayleigh_fading)
-    return h
+    rayleigh_fading = np.random.normal(0, np.sqrt(0.5), size=(d2b.shape[0], M)) + 1j * np.random.normal(0, np.sqrt(0.5), size=(d2b.shape[0], M))
+    H = np.multiply(np.sqrt(PL.reshape(8,1)) , rayleigh_fading)
+    return H
 
-def simulate_noise(snr, N):
+def generate_gaussian_pilots(L, N):
+    std = np.sqrt(1/(2*L))
+    pilots = np.random.normal(0, std, size=(L,N)) + 1j * np.random.normal(0, std, size=(L,N))
+    norms = np.linalg.norm(pilots, axis=0)
+    p_n = pilots/norms
+    return p_n
+
+def simulate_noise(snr, L, M):
     s_lin = 10 ** (snr / 10)
     n_std = np.sqrt(1 / (2 * s_lin))
-    n = np.random.normal(0, n_std, size=N) + 1j * np.random.normal(0, n_std, size=N)
+    n = np.random.normal(0, n_std, size=(L, M)) + 1j * np.random.normal(0, n_std, size=(L, M))
     return n
--- main.py
+++ main.py
@@ -3,20 +3,23 @@ from cell_functions import * #populate_cell, calculate_distance, simulate_path_l
 
 if __name__ == '__main__':
     #------------ Simulation Parameters --------------
-    cell_radius = 4                                                         #in meters
+    cell_radius = 4                                                                     #in meters
     grid_size = 2*cell_radius + 1
-    sparsity = 0.1
+    sparsity = 0.1                                                                      #N = +- (sparsity * grid_size^2) -> 100
     grid = np.zeros((grid_size, grid_size))
-    freq = 2                                                                #in GHz
-    SNR = 10                                                                #in dB
+    freq = 2                                                                            #in GHz -> w = 15cm, spacing between antenna = 4*w
+    SNR = 10                                                                            #in dB
+    Lp = 3                                                                              #Pilot sequence length L << N -> 12
+    M = 4                                                                               #Nr of antennas
+    grid, indices, indices_list, N = populate_cell(grid, sparsity)
 
-    grid, indices, indices_list = populate_cell(grid, sparsity)
+    distances2b, distance_matrix = calculate_distances(grid, indices, M, N)             #Distances2b for each antenna: dim = NxM
+    H = simulate_path_loss_rayleigh(distances2b,distance_matrix, M, freq)               #H is channel matrix: dim = NxM
+    A = generate_gaussian_pilots(Lp, N)                                                 #A is pilot sequences: dim = LxN
+    No = simulate_noise(SNR, Lp, M)                                                     #No is noise matrix : dim =LxM
+    Y = A @ H + No
 
-    distances2b, distance_matrix = calculate_distances(grid, indices)
-    channel_coeff = simulate_path_loss_rayleigh(distances2b,distance_matrix, freq)
-    noise_vector = simulate_noise(SNR, len(distances2b))
 
-    #TODO create pilots somehow
 
     print(grid)
     print(indices_list)
